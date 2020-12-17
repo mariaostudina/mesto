a11c000dc4caed341a3f538b6c288c221966784d
Maria Ostudina <m.ostudina@gmail.com>
2020-12-17 07:30:03 +0300

    feat: dynamic photos rendering

--- script/index.js
+++ script/index.js
@@ -10,7 +10,7 @@ let popupFormNode = document.querySelector('.popup__form');
 function openPopup() {
     popupNode.classList.add('popup_opened');
     popupNameNode.value = profileTitleNode.textContent;
-    popupDescribtionNode.value = profileSubtitleNode.textContent 
+    popupDescribtionNode.value = profileSubtitleNode.textContent
 }
 
 function closePopup() {
@@ -27,3 +27,54 @@ function submitForm(event) {
 profileEditButtonNode.addEventListener('click', openPopup);
 popupCloseButtonNode.addEventListener('click', closePopup);
 popupFormNode.addEventListener('submit', submitForm);
+
+
+const initialPhotos = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const photosContainerElement = document.querySelector('.photos');
+
+function renderPhotos() {
+
+    let NewHTML = " ";
+
+    newHTML = initialPhotos.map(function (item) {
+        return `<div class="photos__item">
+                <img class="photos__image" src="${item.link}" alt=" ">
+                    <div class="photos__bottom">
+                        <h2 class="photos__caption">${item.name}</h2>
+                        <button class="photos__like-button" type="button" aria-label="like"></button>
+                     </div>
+                </div>`
+    }).join(' ');
+
+    photosContainerElement.insertAdjacentHTML('afterbegin', newHTML);
+}
+
+renderPhotos();
+
+
